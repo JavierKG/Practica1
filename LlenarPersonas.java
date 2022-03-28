377c91fef89daf60aa9ba7ac6e7487df67c026f8
Javier <javier.moncoferet@gmail.com>
2022-03-28 12:07:30 +0200

    Modificado archivo LlenarPersonas.java

--- LlenarPersonas.java
+++ LlenarPersonas.java
@@ -6,16 +6,16 @@ import java.util.ArrayList;
    
    String nombres[] = { "Ana", "Luis", "Alicia", "Pedro", "Carmen", "Andrea", "Cristina", "Julio","Patricia" };
    int edades[] = { 14, 15, 13, 15, 16, 12, 16, 14, 13 };
-
+   String poblaciones[] = { "Castellon", "Nules", "Moncofa", "Madrid", "Caceres", "Murcia", "Sagunto", "Granada","Leon" };
    ArrayList<Persona> lista = new ArrayList<Persona>();
 
    // Llena la lista
    for (int i = 0; i < n; i++){
-      lista.add(new Persona(nombres[i], edades[i]));
+      lista.add(new Persona(nombres[i], edades[i], poblaciones[i]));
    }
 
    // Muestra la lista
    for (int i = 0; i < lista.size(); i++)
-      System.out.println(lista.get(i).getNombre() + "*" + lista.get(i).getEdad());
+      System.out.println(lista.get(i).getNombre() + "*" + lista.get(i).getEdad()+ "*" + lista.getPoblacion());
    }
 }// main
